f4f7fba7d589035f869fa3fc8506c56d2e04fd57
Martha30 <53914325+Martha30@users.noreply.github.com>
2020-04-05 20:53:18 -0600

    Modificaciones

--- app/src/main/java/com/example/imc/MainActivity.kt
+++ app/src/main/java/com/example/imc/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,11 +14,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Button.setOnClickListener(View.OnClickListener{
-            
-        })
+        btnIMC.setOnClickListener(View.OnClickListener {
+            val peso = edtPeso.text.toString().toDouble() / 2.2
 
+            val altura = edtAltura.text.toString().toDouble() /100
+
+            val imc = peso / (altura * altura)
+
+            var diagnostico:String =""
+
+            if (imc < 17)
+            {
+                diagnostico = "Esta muy bajo de peso"
+            }
+            else if (imc < 18.49)
+            {
+                diagnostico = "Esta abajo del peso normal"
+            }
+            else if (imc < 24.99)
+            {
+                diagnostico = "Tiene un peso normal"
+            }
+            else if (imc < 29.99)
+            {
+                diagnostico = "Esta por encima del peso normal"
+            }
+            else
+            {
+                diagnostico = "Tiene Obesidad "
+            }
+
+            Toast.makeText(this, diagnostico, Toast.LENGTH_LONG).show()
+
+        })
 
 
     }
 }
+
