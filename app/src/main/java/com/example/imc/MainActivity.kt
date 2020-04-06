5096d8733353f5bd548425e32ef731591f3bd2ce
Martha30 <53914325+Martha30@users.noreply.github.com>
2020-04-05 19:41:40 -0600

    Initial commit

---
+++ app/src/main/java/com/example/imc/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.imc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Button.setOnClickListener(View.OnClickListener{
+            
+        })
+
+
+
+    }
+}
